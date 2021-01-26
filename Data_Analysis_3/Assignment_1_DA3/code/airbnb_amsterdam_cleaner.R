9ed1c3aba0e9e6f5e03ceedd4814595c8e449581
Fasih Atif <fasihatif@gmail.com>
2021-01-26 23:16:04 +0100

    Updated function code

--- Data_Analysis_3/Assignment_1_DA3/code/airbnb_amsterdam_cleaner.R
+++ Data_Analysis_3/Assignment_1_DA3/code/airbnb_amsterdam_cleaner.R
@@ -46,12 +46,24 @@ data <- data %>% select(-contains(c("]","[")))
 
 # Function which merges several columns of same type/category into one generic binary column
 
-dummy_category <- function(word,df_name,df_name_merge){
+dummy_category <- function(word){
+  
+  # Subset columns which contains a specific word and save them to anther dataframe. Also select 'id' to use for merge later
   df_name <- data %>% select(contains(word),"id")
+  
+  #Go row by row to see if any of the rows have a 1. If it does, populate new column 'col_name' with 1
   df_name$col_name <- apply(df_name[0:ncol(df_name)], 1, function(x) ifelse(any(x == 1), '1', '0'))
+  
+  # Save new column and id column to another dataframe. We use this new dataframe to merge with original dataframe
   df_name_merge <- df_name %>% select(id,col_name)
+  
+  #merge original dataframe and df_name_merge by 'id'
   data <- merge(data,df_name_merge,by = "id", all = FALSE)
+  
+  #remove the new column and 'id' column from the df_name dataframe
   df_name <- df_name %>% select(-c(id,col_name))
+  
+  # Remove the selected columns from original dataframe since they have already been aggragated into a new column and merged
   data <<- data %>% select(-colnames(df_name))
 }
 
@@ -59,28 +71,29 @@ dummy_category <- function(word,df_name,df_name_merge){
 df_backup <- data
 data <- df_backup
 
+### Updated dummy_category code
 # Combine all sound system columns into 1 column.There are several different kinds of sound systems present.We would like to
 # create one generic sound category.
 
-dummy_category("sound", sound_df,sound_df_merge)
+dummy_category("sound")
 data <- data %>% rename("sound_system" = col_name)
 
-dummy_category("stove",stove_df,stove_df_merge)
+dummy_category("stove")
 data <- data %>% rename("stove_gas_or_elect" = col_name)
 
-dummy_category("Wifi",wifi_df,wifi_df_merge)
+dummy_category("Wifi")
 data <- data %>% rename("wifi" = col_name)
 
-dummy_category("TV",TV_df,TV_df_merge)
+dummy_category("TV")
 data <- data %>% rename("tv" = col_name)
 
-dummy_category("oven",oven_df,oven_df_merge)
+dummy_category("oven")
 data <- data %>% rename("oven" = col_name)
 
-dummy_category("refrigerator",refrigerator_df,refrigerator_df_merge)
+dummy_category("refrigerator")
 data <- data %>% rename("refrigerator" = col_name)
 
-dummy_category("Paid parking",paid_park_df,paid_park_df_merge)
+dummy_category("Paid parking")
 data <- data %>% rename("paid_parking" = col_name)
 
 data_out <- "C:/Users/abc/OneDrive/Business_Analytics/Data-Analysis-1-2-3/Data_Analysis_3/Assignment_1_DA3/data/"
