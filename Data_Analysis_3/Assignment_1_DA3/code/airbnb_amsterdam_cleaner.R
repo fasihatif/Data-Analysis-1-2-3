f1fcb3ad43970ebed016ba3c4fff9c4073d49d38
Fasih Atif <fasihatif@gmail.com>
2021-01-28 20:31:47 +0100

    Cleaning in progress

--- Data_Analysis_3/Assignment_1_DA3/code/airbnb_amsterdam_cleaner.R
+++ Data_Analysis_3/Assignment_1_DA3/code/airbnb_amsterdam_cleaner.R
@@ -1,18 +1,24 @@
 
-# Fix link
-data = read.csv("https://raw.githubusercontent.com/fasihatif/Data-Analysis-1-2-3/master/Data_Analysis_3/Assignment_1_DA3/data/listings.csv")
-data <- listings
-
 library(tidyverse)
 library(data.table)
 
-
+# Fix link
+listings = read.csv("https://raw.githubusercontent.com/fasihatif/Data-Analysis-1-2-3/master/Data_Analysis_3/Assignment_1_DA3/data/listing.csv")
+data <- as.data.frame(listings)
+glimpse(data)
 ###################
 #  Data Cleaning  #
 ###################
 
-# Drop unnecessary columns
+# Filter for apartments which accomodate 2-6 persons
+properties_type <- as.data.frame(table(data$property_type))
 
+data <- data %>%
+  filter(property_type %in% c("Entire apartment", "Entire serviced apartment")) %>%
+  filter(between(accommodates, 2, 6))
+
+
+# Drop unnecessary columns
 data <- data[grep("^host", colnames(data), invert = TRUE)]
 data <- data[grep("^calculated", colnames(data), invert = TRUE)]
 data <- data %>% select(-contains("maximum"))
@@ -21,19 +27,11 @@ data <- data %>% select(-c("listing_url","scrape_id","last_scraped","name","desc
                            "calendar_last_scraped","number_of_reviews_ltm","number_of_reviews_l30d","license","reviews_per_month",
                            "availability_30","availability_60","availability_90","availability_365","neighbourhood","has_availability"))
 
-data <- data %>% select(-`  bluetooth connection for you to connect your Spotify sound system with Bluetooth and aux`)
-
-#remove dollar signs from price variable
-data$price<-gsub("\\$","",as.character(data$price))
-data$price<-as.numeric(as.character(data$price))
-
-
-#amenities
+# Split the amenities into separate binary columns
 data$amenities<-gsub("\\[","",data$amenities)
 data$amenities<-gsub("\\]","",data$amenities)
 data$amenities<-gsub('\\"',"",data$amenities)
 data$amenities<-as.list(strsplit(data$amenities, ","))
-
 
 #define levels and dummies 
 levs <- levels(factor(unlist(data$amenities)))
@@ -41,16 +39,34 @@ data<-cbind(data,as.data.frame(do.call(rbind, lapply(lapply(data$amenities, fact
 
 # data <- data %>% select(-contains(c("]","[")))
 
-#Checkpoint
-aggregate_backup <- data
-data <- aggregate_backup
+#Checkpoint 1: Initial column cleaning
+backup_a <- data
+data <- backup_a
+
+
+# data <- data %>% select(-c("Amazon Video Prime", Mini fridge", "and wardrobe","Changing table","Children\u2019s books and toys",
+# "Children\u2019s dinnerware","Complimentary self parking", "Free driveway parking on premises \u2013 1 space",
+# "Free parking on premises \u2013 1 space", "Free residential garage on premises", "Radiant heating",
+# "Valet parking \u2014 \u20ac35/day","Babysitter recommendations","-`  bluetooth connection for you to connect your Spotify sound system with Bluetooth and aux`",
+# "Luggage store possible ( small fee)// * I wash your dishes. Enjoy holiday!//* My fridge in kitchen","L'Oreal","portable"))
 
 ### Updated aggregate_columns function code ###
 # Example: Combine all sound system columns into 1 column.There are several different kinds of sound systems present.We would like to
 # create one generic sound category.
 
-# Pass a vector of phrases to make the process quicker
-column_names <- c("stove","Wifi","TV","oven","frige","Paid parking", "soap","shampoo","Self-parking","washer","toiletries","conditioner")
+data <- data %>% mutate(
+  "Mini frige" = "Mini fridge",
+  "Nespresso coffee machine" = "Nespresso machine",
+  "Shower gel soap" = "Shower gel",
+  "BBQ utensils" = "Barbecue utensils",
+  "Freezer frige" = "Freezer",
+  "free garage parking" = "` Free residential garage on premises`",
+  "Mini frige" = "Mini fridge"
+)
+
+
+# Pass a vector of phrases to the for loop to make the process quicker
+column_names <- c("sound", "stove","Wifi","TV","oven","frige", "soap", "BBQ", "toys", "crib", "parking", "shampoo", "heating","washer","toiletries","conditioner","dry")
 
 for( word in column_names){
   
@@ -71,40 +87,48 @@ for( word in column_names){
   
   # Remove the subset columns from original dataframe since they have already been aggregated into a new column and merged
   data <- data %>% select(-colnames(new_df))
+  
+  # Convert from character to integer
   data$col_name <- as.integer(data$col_name)
+  
+  # Rename new column
   names(data)[names(data) == 'col_name'] <- paste0(word,"_agg")
   
 }
 
-# Function version
-aggregate_columns("sound")
-data <- data %>% rename("sound_system" = col_name)
-data$sound_system <- as.integer(data$sound_system)
+# contains_list <- c("breakfast")
+# data <- data %>% select(-contains(contains_list))
 
-contains_list <- c("breakfast", "L'Oreal","portable")
-data <- data %>% select(-contains(contains_list))
+# Checkpoint 2:Subset data further for cleaning
+backup_b <- data
+data <- backup_b
 
-# Experimenting
-count_df <- sapply(data[75:227], function(x){sum(x)})
+count_df <- sapply(data[25:152], function(x){sum(x)})
 count_df <- data.frame(count_df)
 
-data <- data %>% data[, -which(numcolwise(sum(data) < 10))]
+# Subset all ameneties columns and remove any which have '1' less than 5%
+amenities_clean <- data %>% select(25:152, "id")
+less_than_5per <- amenities_clean %>% select(where(~mean(. == 1) <= 0.005))
+less_than_5per <- less_than_5per %>% select(-contains(c("id")))
+amenities_clean <- amenities_clean %>% select(-colnames(less_than_5per))
 
+# Check for count
+amenities_clean_df <- sapply(amenities_clean, function(x){sum(x)})
+amenities_clean_df <- data.frame(amenities_clean_df)
 
-# checkpoint
-df_backup <- data
-data <- df_backup
+# Merge the original and amenities dataframe
+data <- data %>% select(-(25:152))
+data <- merge(data,amenities_clean, by = "id", all = TRUE)
 
-### Updated aggregate_columns function code ###
-# Combine all sound system columns into 1 column.There are several different kinds of sound systems present.We would like to
-# create one generic sound category.
+#remove dollar signs from price variable. These prices are actually Euros
+data$price<-gsub("\\$","",as.character(data$price))
+data$price<-as.numeric(as.character(data$price))
 
-aggregate_columns("sound")
-data <- data %>% rename("sound_system" = col_name)
-data$sound_system <- as.integer(data$sound_system)
+?mutate
 
+data <- data %>% select(-c("amenities", "Babysitter recommendations", "Baby bath", "Baking sheet", "` Children\u2019s dinnerware`"))
 
-
+names(data)[names(data) == "frige_agg"] <- "Refridgerator"
 
 data_out <- "C:/Users/abc/OneDrive/Business_Analytics/Data-Analysis-1-2-3/Data_Analysis_3/Assignment_1_DA3/data/"
 write.csv(data,file=paste0(data_out,"amsterdam_amenties.csv"), row.names = FALSE)
