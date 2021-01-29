7a9876025d7456a86d1025f393ea51d30054adc6
Fasih Atif <fasihatif@gmail.com>
2021-01-29 11:37:59 +0100

    Save initial prediction script for amsterdam airbnb

---
+++ Data_Analysis_3/Assignment_1_DA3/code/airbnb_amsterdam_pred.R
@@ -0,0 +1,7 @@
+
+library(tidyverse)
+library(caret)
+
+df <- read.csv("https://raw.githubusercontent.com/fasihatif/Data-Analysis-1-2-3/master/Data_Analysis_3/Assignment_1_DA3/data/amsterdam_clean.csv")
+
+# Comment: Start with prediction
